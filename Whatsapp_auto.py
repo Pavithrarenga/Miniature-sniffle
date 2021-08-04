ef7257430bf774324c8e069daf2c4c3d380a4341
Pavithrarenga <31246588+Pavithrarenga@users.noreply.github.com>
2021-08-04 07:50:12 +0530

    Send whatsapp message using Python
    Automate Whatsapp using Python

---
+++ Whatsapp_auto.py
@@ -0,0 +1,6 @@
+import pywhatkit as kit
+
+# First argument is the number
+# Second argument is the message
+# Third and Fourth argument is the time in hr and min
+kit.sendwhatmsg("+918608120713", "Hello. ", 19, 41)
